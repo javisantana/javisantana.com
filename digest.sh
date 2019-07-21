d4e1c52aed7e19b734f2d204bdbcf2fe4f87136b
javi <qualopec@gmail.com>
2019-07-21 23:56:40 +0200

    digest

---
+++ digest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export DIGEST_BRANCH=digest_`date +"%d-%m-%Y"`
+git checkout -b $DIGEST_BRANCH
+
+# create the markdown with the digest
+echo "---" > _digest/$DIGEST_BRANCH.md
+echo "layout: micro_post" >> _digest/$DIGEST_BRANCH.md
+echo "ublished: true" >> _digest/$DIGEST_BRANCH.md
+echo "name: Javisantana.com, resumen `date +"%d-%m-%Y"`" >> _digest/$DIGEST_BRANCH.md
+echo "---" >> _digest/$DIGEST_BRANCH.md
+echo "" >> _digest/$DIGEST_BRANCH.md
+echo "# Resumen semana" >> _digest/$DIGEST_BRANCH.md
+
+git log --since=1.week --reverse --pretty=format:'## %s%n%b ' >> _digest/$DIGEST_BRANCH.md
+#--grep="^D:
+
+git add _digest/$DIGEST_BRANCH.md
+git commit -m "digest for `date +"%d-%m-%Y"`"
+git push origin $DIGEST_BRANCH
+#git request-pull -p origin/gh-pages $DIGEST_BRANCH 
+#https://github.com/javisantana/javisantana.com
+git checkout gh-pages
+
